0cb77d15212a07e81d13361003572c13712332da
vaishnavipendse <45134841+vaishnavipendse@users.noreply.github.com>
2018-12-02 16:08:38 -0800

    Update updated_algo.py

--- updated_algo.py
+++ updated_algo.py
@@ -1,39 +1,40 @@
 probabilities = {}
 import sys
+import copy
+
 def propogation(UCQ, quantifier, sep, list_of_separator_variables):
-    common_table,sep_table_list = find_Common_Table(list_of_separator_variables) # tables whose probabilities can be substituted directly
+    common_table, sep_table_list = find_Common_Table(list_of_separator_variables)  # tables whose probabilities can be substituted directly
     #remove=[]
     #for i in range(0,len(UCQ)):  # to track which common table names need to be removed from each conjunctive query
-        #subremove = []
-        #for table in UCQ[i]:
-            #if table in common_table:
-                #subremove.append(table)
-        #remove.append(subremove)
-    #for i in range(0,len(remove)):# remove common table names from each conjunctive query
-        #for j in remove[i]:
-            #del UCQ[i][j]
-            #if (len(UCQ[i]) == 0):
-                #UCQ.remove(UCQ[i])
-                #i -= 1
-    return UCQ, quantifier, common_table,sep_table_list
-
+    subremove = []
+    #for table in UCQ[i]:
+    #if table in common_table:
+    #subremove.append(table)
+    #remove.append(subremove)
+    # for i in range(0,len(remove)):# remove common table names from each conjunctive query
+    # for j in remove[i]:
+    # del UCQ[i][j]
+    # if (len(UCQ[i]) == 0):
+    # UCQ.remove(UCQ[i])
+    # i -= 1
+    return UCQ, quantifier, common_table, sep_table_list
 
 def find_Common_Table(list_of_separator_variables):  # find tables whose probabilities can be substituted directly
-    intersection=True
-    common_table=set()
-    sep_table_list={} #To store those tables' information that are being removed from UCQ
+    intersection = True
+    common_table = set()
+    sep_table_list = {}  # To store those tables' information that are being removed from UCQ
     # Check intersection of all CQs only if number of CQs is > 1
-    if(len(tables)>1):
+    if (len(tables) > 1):
         common_table = tables[0]
         for i in range(1, len(tables)):
             common_table = common_table.intersection(tables[i])
-        if(len(common_table) == 0):
-            intersection=False;
-        if(intersection==True):
+        if (len(common_table) == 0):
+            intersection = False;
+        if (intersection == True):
             for t in common_table:
                 sep_table_list[t] = UCQ[0].get(t)
     # If intersection is a null set or number of CQs is 1 then find a table with separator variable
-    if(intersection==False or len(tables)==1):
+    if (intersection == False or len(tables) == 1):
         for i in range(0, len(UCQ)):
             for table_name in tables[i]:
                 append = True
@@ -43,11 +44,10 @@ def find_Common_Table(list_of_separator_variables):  # find tables whose probabi
                         continue
                     else:
                         append = False
-                if (append==True and table_name not in common_table):
+                if (append == True and table_name not in common_table):
                     common_table.add(table_name)
                     sep_table_list[table_name] = UCQ[i].get(table_name)
-    return (common_table,sep_table_list)
-
+    return (common_table, sep_table_list)
 
 def find_Separator(UCQ, quantifier):  # find separator for entire UCQ
     clause_count = 0
@@ -66,7 +66,6 @@ def find_Separator(UCQ, quantifier):  # find separator for entire UCQ
             quantifier[q] = 0
             return q
 
-
 def check_Independence_CQ(cq):  # check independence within a CQ
     tables = set()
     for clause in cq:
@@ -75,18 +74,15 @@ def check_Independence_CQ(cq):  # check independence within a CQ
         tables.add(cq[clause].get("name"))
     return True
 
-
 def check_Independence_UCQ(UCQ):  # Check independence across entire UCQ, i.e. no repeating table names
     temp = set()
     for cq in range(len(UCQ)):
-        for k in cq.keys():
+        for k in UCQ[cq].keys():
             if k in temp:
                 return False
             else:
                 return True
-        
     return True
-
 
 def no_existential_quantifier(sub_CQ, quantifier):
     # Check if the none of the variables in the UCQ are existential in quantifier dict.
@@ -96,14 +92,12 @@ def no_existential_quantifier(sub_CQ, quantifier):
             return False
     return True
 
-
 def update_quantifiers(sub_CQ, quantifiers):
     print("SUBCQ-up")
     for vars in sub_CQ["var"]:
         if (quantifier[vars] == 1):
             quantifier[vars] = 0
     return quantifiers
-
 
 def test(UCQ):
     for value in UCQ:
@@ -113,59 +107,72 @@ def test(UCQ):
             return True
     return False
 
-
 def isUCQ(UCQ):
     if type(UCQ) is list:
         return True
     elif type(UCQ) is dict:
         return False
 
-
-def getProbability(sep_table_list):
-    print("getProbability",sep_table_list)
+def getProbability(UCQ,sep_table_list,quantifiers,sep,tables):
+    print("getProbability", sep_table_list)
     print(probabilities)
-    length=sys.maxsize
+    length = sys.maxsize
     for k in sep_table_list:
-        length=min(length,len(probabilities[k])) #To check the minimum no. of rows across all tables
-    ans=1
+        length = min(length, len(probabilities[k]))  # To check the minimum no. of rows across all tables
+    ans = 1
     # Iterating through each row
     for i in range(length):
-        term=1
-        #Iterating through each table for a row
+        term = 1
+        #Substitute value of constant in each table's var list in place of sep variable
+        UCQ1=copy.deepcopy(UCQ)
+        for cq in UCQ1:
+            for t in cq.keys():
+                cq[t]["const"]=True
+                temp=cq[t]["var"]
+                for s in temp:
+                    if(sep==s):
+                        temp[temp.index(s)]=str(i)
+        # Iterating through each table for a row
         for k in sep_table_list:
-            if(sep_table_list[k]["negation"]==False):
-                term=term*probabilities[k][i]
+            if (sep_table_list[k]["negation"] == False):
+                term = term * probabilities[k][i]*probability(UCQ1,quantifiers,tables)
             else:
-                term=term*(1-probabilities[k][i])
-        ans=ans*(1-term)
+                term = term * (1 - (probabilities[k][i])*probability(UCQ1,quantifiers,tables))
+        ans = ans * (1 - term)
     return ans
 
-def allConstantParameters(subUCQ): #checks if all variables are numbers and not characters
+def allConstantParameters(subUCQ):  # checks if all variables are numbers and not characters
     for x in subUCQ["var"]:
-        if(x.isdigit()==False):
+        if (x.isdigit() == False):
             return False
     return True
 
 def probability(UCQ, quantifiers, tables):
-    #Base of recursion
-    if(len(UCQ)==1 and len(UCQ[0])==1 and allConstantParameters(UCQ[0][list(UCQ[0].keys())[0]])): #is a ground atom
+    print(UCQ)
+    sep=""
+    # Base of recursion
+    if (len(UCQ) == 1 and len(UCQ[0]) ==1 and allConstantParameters(UCQ[0][list(UCQ[0].keys())[0]])):  # is a ground atom
         print("CASE1")
-        return getProb(UCQ[0][list(UCQ[0].keys())[0]]) #checks if the given constant values are present in the given tables, if present return probability, else returns 0
-    #convert to ucnf
-    if(len(UCQ)==2 and check_Independence_UCQ(UCQ)): #both cq are independent of each other
+        return getProbability(UCQ,UCQ[0][list(UCQ[0].keys())[0]],quantifiers,sep,tables)# checks if the given constant values are present in the given tables, if present return probability, else returns 0
+    # convert to ucnf
+    if (len(UCQ) == 2 and check_Independence_UCQ(UCQ)):  # both cq are independent of each other
         print("CASE2")
-        return 1 - ((1 - probability(UCQ[0]))*(1- probability(UCQ[1])))
-    if(check_Independence_UCQ(UCQ)): #check fi all cq are independent 
-        print("CASE3")
-        sum = 0
-        return -2
-        #not sure how to translate this formula. what is m? why have they sued subset and not belongs
-    if(len(UCQ)==0 and check_Independence_CQ(UCQ[0])):
+        return 1 - ((1 - probability(UCQ[0])) * (1 - probability(UCQ[1])))
+    if (len(UCQ)>2):
+        if(check_Independence_UCQ(UCQ)):  # check if all cq are independent
+            print("CASE3")
+            sum = 0
+            return -2
+        # not sure how to translate this formula. what is m? why have they sued subset and not belongs
+    if (len(UCQ) == 0 and check_Independence_CQ(UCQ[0])):
         print("CASE4")
-        return probability(UCQ[0][UCQ[0][list(UCQ[0].keys())[0]]])* probability(UCQ[0][UCQ[0][list(UCQ[0].keys())[1]]])
+        return probability(UCQ[0][UCQ[0][list(UCQ[0].keys())[0]]]) * probability(UCQ[0][UCQ[0][list(UCQ[0].keys())[1]]])
     sep = (find_Separator(UCQ, quantifiers))
-    if(sep is not None):
-        #translate algo
+    if (sep is not None):
+        list_of_separator_variables=[]
+        list_of_separator_variables.append(sep)
+        UCQ,quantifiers,common_table,sep_table_list=propogation(UCQ, quantifiers, sep, list_of_separator_variables)
+        print(getProbability(UCQ,sep_table_list,quantifiers,sep,tables))
         return -3
     return -1
 def parse_UCQ(input_query):
@@ -195,21 +202,14 @@ def parse_UCQ(input_query):
         UCQ.append(dict_cq)  # append conjunctive clause dictionary to the list of union of conjunctive clauses
         tables.append(temp_tables)
     return UCQ, quantifier, tables
-
-
-input_query = "S(x)"
-UCQ, quantifier, tables = parse_UCQ(input_query)
-print(UCQ)
-probabilities = {'S':[0.8,0.2,0.3], 'R':[0.3,0.4,0.9]}
-#probabilities = {'S': [[[0,1],0.8],[[1,0],0.2], [[0,0],0.3]], 'R':[[[0,1],0.3],[[1,0],0.4],[[0,0],0.9]]}
+input_query = "S(x),R(x,y)"
+UCQ,quantifier,tables = parse_UCQ(input_query)
+probabilities = {'S': [0.8, 0.2, 0.3], 'R': [0.3, 0.4, 0.9]}
+# probabilities = {'S': [[[0,1],0.8],[[1,0],0.2], [[0,0],0.3]], 'R':[[[0,1],0.3],[[1,0],0.4],[[0,0],0.9]]}
 print(probability(UCQ, quantifier, tables))
+
 # 1 - 0.09 = 0.91
 # 1 - 0.08 = 0.92
 # 1 - 0.24 = 0.76
 # 0.636
 # 1- 0.636 = 0.364
-
-
-
-
-
